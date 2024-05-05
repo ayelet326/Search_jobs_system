9ef0f7ddb5049d939d9e25c728679a1f49628063
Ayelet <ayelet203320@gmail.com>
2024-05-06 01:15:57 +0300

    server side: change string validation

--- server side/Validation/ValidationFunctions.cs
+++ server side/Validation/ValidationFunctions.cs
@@ -7,6 +7,6 @@ public class ValidationFunctions
     // String validation logic here
     public static bool IsValidString(string? name)
     {
-        return !string.IsNullOrWhiteSpace(name) && Regex.IsMatch(name, @"^[a-zA-Z\s]+$");
+        return !string.IsNullOrWhiteSpace(name) && Regex.IsMatch(name, @"^[a-zA-Z0-9\s+\-\/\']+$");
     }
 }
