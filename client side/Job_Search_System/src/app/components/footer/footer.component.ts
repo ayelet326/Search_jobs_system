7da9effa863c088bd7c2f2e9ed29e6ed9fcd6290
Ayelet <ayelet203320@gmail.com>
2024-05-05 22:44:42 +0300

    Add footer component

--- client side/Job_Search_System/src/app/components/footer/footer.component.ts
+++ client side/Job_Search_System/src/app/components/footer/footer.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  JoinToNewsletter(emailInput:string){
+alert("you joined in succesful!")
+  }
 }
