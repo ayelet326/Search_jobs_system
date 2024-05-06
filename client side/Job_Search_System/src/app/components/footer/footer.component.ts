d9c32b0f1eca528a0d760c93a38f64d5822d3e17
Ayelet <ayelet203320@gmail.com>
2024-05-06 03:20:33 +0300

    Add validateEmail

--- client side/Job_Search_System/src/app/components/footer/footer.component.ts
+++ client side/Job_Search_System/src/app/components/footer/footer.component.ts
@@ -7,7 +7,15 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
-  JoinToNewsletter(emailInput:string){
-alert("you joined in succesful!")
+  JoinToNewsletter(emailInput: string) {
+    if (this.validateEmail(emailInput))
+      alert("You joined in successful!")
+    else
+    alert("Your email not valid!")
+  }
+
+  validateEmail(email: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
   }
 }
