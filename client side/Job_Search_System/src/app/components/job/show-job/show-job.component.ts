43d06a5d8e2fd14b110e7f05f6510cddfc3183ce
Ayelet <ayelet203320@gmail.com>
2024-05-06 03:07:48 +0300

    add show-job component

--- client side/Job_Search_System/src/app/components/job/show-job/show-job.component.ts
+++ client side/Job_Search_System/src/app/components/job/show-job/show-job.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { JobFieldService } from '../../../services/jobField.service';
+import { UserService } from '../../../services/user.service';
+import { Job } from '../../../models/job';
+import { JobField } from '../../../models/jobField';
 
 @Component({
   selector: 'app-show-job',
   templateUrl: './show-job.component.html',
-  styleUrl: './show-job.component.css'
+  styleUrl: './show-job.component.scss'
 })
-export class ShowJobComponent {
+export class ShowJobComponent implements OnInit {
+  @Input() job: Job | undefined;
+  jobFieldName?: string;
+  showDetails: boolean = false;
 
+  constructor(private jobFieldService: JobFieldService, private userService: UserService) { }
+
+  ngOnInit() {
+    if (this.job?.jobFieldId) {
+      this.jobFieldService.getJobFieldById(this.job.jobFieldId).subscribe((jobField: JobField | undefined) => {
+        this.jobFieldName = jobField?.jobFieldName;
+      });
+    }
+  }
+  toggleDetails() {
+    this.showDetails = !this.showDetails;
+  }
+  SentCV(jobId:number  ) {
+    this.userService.addJob(jobId);
+    alert("CV sent successfuly");
+  }
 }
