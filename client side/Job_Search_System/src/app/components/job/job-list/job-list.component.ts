a5fb16db549145d72500961832b6ca4eaada8673
Ayelet <ayelet203320@gmail.com>
2024-05-06 03:08:12 +0300

    add job-list component

--- client side/Job_Search_System/src/app/components/job/job-list/job-list.component.ts
+++ client side/Job_Search_System/src/app/components/job/job-list/job-list.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Job } from '../../../models/job';
+import { JobService } from '../../../services/job.service';
 
 @Component({
   selector: 'app-job-list',
   templateUrl: './job-list.component.html',
-  styleUrl: './job-list.component.css'
+  styleUrl: './job-list.component.scss'
 })
-export class JobListComponent {
+export class JobListComponent implements OnInit {
+  jobs: Job[] | undefined;
+  @Input() jobFieldId?: number;
+  @Input() jobArea?: string;
 
+  constructor(private jobService: JobService) { }
+
+  ngOnInit() {
+    this.getJobs();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.getJobs();
+    if (changes['jobFieldId'] || changes['jobArea']) {
+      this.filterJobs();
+    }
+  }
+
+
+  getJobs(): void {
+    this.jobService.getJobList()
+      .subscribe((jobs: Job[]) => {
+        this.jobs = jobs;
+        this.filterJobs();
+      });
+  }
+
+  filterJobs(): void {
+    if (this.jobFieldId) {
+      this.jobs = this.jobs?.filter(job => job.jobFieldId == this.jobFieldId);
+    }
+    if (this.jobArea) {
+      this.jobs = this.jobs?.filter(job => job.area.toLowerCase().includes(this.jobArea!.toLowerCase()));
+    }
+  }
 }
