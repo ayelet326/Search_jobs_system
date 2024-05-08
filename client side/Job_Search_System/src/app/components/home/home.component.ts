24ea568c24aa8efad1348206d6033d49c25e7e52
Ayelet <ayelet203320@gmail.com>
2024-05-08 05:35:33 +0300

    fix home component

--- client side/Job_Search_System/src/app/components/home/home.component.ts
+++ client side/Job_Search_System/src/app/components/home/home.component.ts
@@ -1,45 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { JobField } from '../../models/jobField';
-import { JobFieldService } from '../../services/jobField.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
-  jobFields: JobField[] = [];
-  selectedFieldId?: number;
-  selectedArea?: string;
-  jobFieldId?: number;
-  jobArea?: string;
+export class HomeComponent{
 
-  constructor(private jobFieldService: JobFieldService) { }
 
-  ngOnInit() {
-    this.getJobFields();
+  constructor() { }
 
-  }
-
-  getJobFields(): void {
-    this.jobFieldService.getJobFields()
-      .subscribe((jobFields: JobField[]) => {
-        this.jobFields = jobFields;
-      });
-  }
-
-  onSearch(): void {
-    if ((this.selectedFieldId !== undefined && this.selectedFieldId?.toString() !== "") || this.selectedArea) {
-      this.jobFieldId = this.selectedFieldId || undefined;
-      this.jobArea = this.selectedArea || undefined;
-    }
-  }
-  onJobFieldLinkClicked(jobFieldId: number | undefined): void {
-    console.log(jobFieldId);
-    if (jobFieldId !== this.selectedFieldId) {
-      this.selectedFieldId = jobFieldId;
-      this.onSearch();
-    }
-  }
-  
 }
